64edd5ea8f54a7e926882eb75b70fc513f10befd
Ubaldo <150012434+UbaldoCap@users.noreply.github.com>
2024-02-16 14:49:42 +0100

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo1/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class HomeController {
+    @GetMapping("/ciao")
+    public String ciao(@RequestParam String nome, @RequestParam String provincia) {
+        return "Ciao " + nome + ", com'è il tempo in " + provincia + "?";
+    }
+    //http://localhost:8080/v1/ciao?nome=Giuseppe&provincia=Lombardia
+}
